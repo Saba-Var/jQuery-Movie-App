d8661e60e325b90fbeba560358cb0dc479dbb10b
Saba-Var <vartasashvili94@gmail.com>
2022-11-27 20:35:14 +0400

    feat: toggle play icon when hover on the movie card

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -118,21 +118,30 @@ jQuery(() => {
         movie.backdrop_path ? movie.backdrop_path : movie.poster_path
       )
 
-      $(id).append(`<li
+      $(id).append(
+        `<li
+      id="item-card"
       class="relative scale lg:mb-6 border-[3px] border-main-red rounded-lg overflow-hidden pb-3"
-    >
+      >
       <a href="/pages/movies/info/index.html?id=${movie.id}&type=${type}">
         <div
-          class="group aspect-w-10 aspect-h-7 block w-full border-none overflow-hidden bg-gray-100"
+          class="group pointer-events-none object-cover group-hover:brightness-50 relative aspect-w-10 aspect-h-7 block w-full border-none overflow-hidden bg-gray-100"
         >
-          <img
-            class="pointer-events-none object-cover group-hover:brightness-50"
+           <img
             src="${background}?api_key=${theMovieDbConfig.apiKey}"
             alt="${movie?.title}"
+            id="background-image"
           />
-          <button type="button" class="absolute inset-0 focus:outline-none">
-            <span class="sr-only">View details for IMG_4985.HEIC</span>
-          </button>
+
+        <div
+        id="play-icon-container"
+        class="absolute h-20 w-20 hide sm:h-28 sm:w-28 lg:h-40 lg:w-40 z-[99] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" >
+        <img
+          id="play-icon"
+          src="../../assets/images/play-icon.png" 
+          alt="play icon" 
+        />
+      </div>
         </div>
         <div class="px-2 sm:px-3">
           <p
@@ -154,7 +163,8 @@ jQuery(() => {
           </div>
         </div>
       </a>
-    </li>`)
+    </li>`
+      )
     })
   }
 })
